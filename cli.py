22d0f66bc223c569d320dcfc8f6b9bc8a78b54d2
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-04-18 20:25:27 +0200

    primeros pasos

---
+++ cli.py
@@ -0,0 +1,13 @@
+import socket
+
+# El cliente debe tener las mismas especificaciones del servidor
+host = socket.gethostname()
+port = 12345
+BUFFER_SIZE = 1024
+MESSAGE = 'Hola, mundo!' # Datos que queremos enviar
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
+    socket_tcp.connect((host, port))
+    # Convertimos str a bytes
+    socket_tcp.send(MESSAGE.encode('utf-8'))
+    data = socket_tcp.recv(BUFFER_SIZE)
