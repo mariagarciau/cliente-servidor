3dfc9d62244c68fd67a93c13c20a3b69b145df7a
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-04-21 19:36:40 +0200

    serv

--- cli.py
+++ cli.py
@@ -11,3 +11,31 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
     # Convertimos str a bytes
     socket_tcp.send(MESSAGE.encode('utf-8'))
     data = socket_tcp.recv(BUFFER_SIZE)
+
+
+#Variables
+host = 'localhost'
+port = 8050
+#Se importa el módulo
+import socket
+ 
+#Creación de un objeto socket (lado cliente)
+obj = socket.socket()
+ 
+#Conexión con el servidor. Parametros: IP (puede ser del tipo 192.168.1.1 o localhost), Puerto
+obj.connect((host, port))
+print("Conectado al servidor")
+ 
+#Creamos un bucle para retener la conexion
+while True:
+    #Instanciamos una entrada de datos para que el cliente pueda enviar mensajes
+    mens = raw_input("Mensaje desde Cliente a Servidor >> ")
+ 
+    #Con el método send, enviamos el mensaje
+    obj.send(mens)
+
+#Cerramos la instancia del objeto servidor
+obj.close()
+
+#Imprimimos la palabra Adios para cuando se cierre la conexion
+print("Conexión cerrada")
